8c67568515c2c21e07be7d7ca53f2af3ad6d8d9f
“mytheart” <550703617@qq.com>
2020-10-02 22:29:10 +0800

    ref:demo

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,28 +1,14 @@
-// 输入: [1, 2, 3, 2, 2, 2, 5,4,4,4,4,4,4, 4, 2]
-// 输出: 2
+// https://leetcode-cn.com/problems/coin-change/
 
-// 思路
-// count 出现的次数 1
-// 出现次数最多的数字 num = nums[0]
+// 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。
+// 如果没有任何一种硬币组合能组成总金额，返回 -1。
 
-function majorityElement(nums: number[]): number {
-  let count = 1,
-    num = nums[0];
+/**
+ * 输入: coins = [1, 2, 5], amount = 11
+ * 输出: 3
+ * 解释: 11 = 5 + 5 + 1
+ */
 
-  for (let i = 0; i < nums.length; i++) {
-    if (count === 0) {
-      num = nums[i];
-      count = 1;
-    }
+// const result = _coinChange(coins, amount)
 
-    if (num === nums[i]) {
-      count++;
-    } else {
-      count--;
-    }
-  }
-
-  return num;
-}
-
-console.log(majorityElement([1, 2, 3, 2, 2, 2, 5, 4, 4, 4, 4, 4, 4, 4, 2]));
+// console.log(coinChange([25, 10, 5, 1], 46))
