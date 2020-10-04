851ab0ea27a70a3d31ad8ab65361951a1ba1155a
“mytheart” <550703617@qq.com>
2020-10-04 20:18:59 +0800

    ref:递归划分到分治算法

--- src/分治法/index.tsx
+++ src/分治法/index.tsx
@@ -1,1 +1,32 @@
-// 分治算法
+/**
+ * 分治算法
+ * 分治算法一般采用递归或循环的方式，通常需要配合贪心算法去优化分治法的效率
+ */
+
+function fib(n) {
+  var temp = {};
+
+  function count(n) {
+    let result;
+
+    if (temp[n]) {
+      return temp[n];
+    }
+
+    if (n <= 0) {
+      result = 0;
+    } else if (n === 1) {
+      result = 1;
+    } else if (n === 2) {
+      result = 1;
+    } else {
+      result = count(n - 1) + count(n - 2);
+    }
+
+    temp[n] = result;
+
+    return result;
+  }
+
+  return count(n);
+}
