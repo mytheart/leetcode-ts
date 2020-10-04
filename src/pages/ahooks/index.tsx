c0c366ae9bc0cb919573a6c75f8e9677138e5d25
“mytheart” <550703617@qq.com>
2020-10-04 20:17:35 +0800

    feat:新增三个ahooks测试页面

---
+++ src/pages/ahooks/index.tsx
@@ -0,0 +1,35 @@
+import { useRequest } from 'ahooks';
+import Mock from 'mockjs';
+import React from 'react';
+
+async function getEmail(search: string): Promise<string[]> {
+  console.log(search);
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(Mock.mock({ 'data|5': ['@email'] }).data);
+    }, 100);
+  });
+}
+
+export default () => {
+  const { data, loading, run } = useRequest(getEmail, {
+    throttleInterval: 400,
+    manual: true,
+  });
+
+  return (
+    <div>
+      <p>Enter quickly to see the effect</p>
+      <input placeholder="Select Emails" onChange={e => run(e.target.value)} />
+      {loading ? (
+        <p>loading</p>
+      ) : (
+        <ul style={{ marginTop: 8 }}>
+          {data?.map(i => (
+            <li key={i}>{i}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
